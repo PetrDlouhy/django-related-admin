75f64316f1c2790c92c58c57c030d5056b14cdc1
Petr Dlouhý <petr.dlouhy@email.cz>
2015-12-20 11:52:10 +0100

    add testing infrastructure

---
+++ test_app/main/admin.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib import admin
+from .models import Album, Musician
+from related_admin import RelatedFieldAdmin
+
+
+class AlbumAdmin(RelatedFieldAdmin):
+    list_display = ('name', 'artist__first_name', 'artist__last_name')
+
+class MusicianAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Musician, MusicianAdmin)
---
+++ test_app/main/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from related_admin import RelatedFieldAdmin
+from django.db import models
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
