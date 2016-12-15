9d53ff4854dbbe19b26da49a8f8a5c7468ae8120
Petr Dlouhý <petr.dlouhy@email.cz>
2016-12-15 07:24:54 +0100

    bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-related-admin',
-    version='0.3.1',
+    version='0.4.0',
     packages=['related_admin'],
     include_package_data=True,
     description="Allow foreign key attributes in list_display with '__'",
